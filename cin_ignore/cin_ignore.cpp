7184d152323b56a598d8f0fd335a1557f3ec9099
Anxon Pues <43876773+anxonpues@users.noreply.github.com>
2022-04-19 18:21:51 +0200

    Add project files.

---
+++ cin_ignore/cin_ignore.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+//m#include <sstream>
+#include <limits>
+
+int main(int argvCounter, char* argvValues[])
+{
+    /*std::istringstream input("1\n"
+        "some non-numeric input\n"
+        "2\n");*/
+std::cout << "\n\tnumero de argumentos en funcion : " << argvCounter << std::endl;
+for (int i = 0; i < argvCounter; i++)
+    std::cout << "\telemento[" << i << "]= " << argvValues[i] << std::endl;
+std::cout << "\n\n";
+
+    for (;;) {
+        int nada;
+        std::cin >> nada;
+
+        if (std::cin.bad() || std::cin.eof()) 
+        {
+            break;
+        }
+        else if (std::cin.fail()) {
+            std::cin.clear(); // unset failbit
+            std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n'); // skip bad input
+        }
+        else {
+            std::cout << nada << '\n';
+            if (nada == 0)
+                break;
+        }
+    }
+}
